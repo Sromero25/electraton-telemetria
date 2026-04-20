36707a1ab0bb0377304602c520ac78f9ed3bd605
Sromero25 <saulofco25@gmail.com>
2026-04-20 00:56:27 -0600

    Limpieza

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from sensors.sensor import leer_estado
-
-contador = 0
-while contador < 5:
-	print("Lectura",contador + 1)
-	resultado = leer_estado()
-	print("Dato:",resultado)
-	contador = contador + 1
-print("Programa terminado")
